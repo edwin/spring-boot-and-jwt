0302d1f340aca49d5ce5f62a2c88bc9980f7688b
edwin <edwinkun@gmail.com>
2025-10-31 16:52:27 +0700

    Remove unused HttpRequest import from HelloWorldService :broom: :duck:

--- src/main/java/com/edw/service/HelloWorldService.java
+++ src/main/java/com/edw/service/HelloWorldService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.net.http.HttpRequest;
 import java.security.Key;
 
 /**
